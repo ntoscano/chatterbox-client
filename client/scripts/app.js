bb0e19cdf924d3a6e294b68c5b600402eda0588b
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 15:46:25 -0700

    Show messages on screen

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,23 @@
 // YOUR CODE HERE:
 
+var parseUrl = 'https://api.parse.com/1/classes/chatterbox';
+
+var displayMessages = function(messages) {
+  var $messages = $('<div>');
+
+  _(messages).each(function(message) {
+    var $message = $('<p>');
+    $message.text(message.text);
+    $messages.append($message);
+  });
+  $('#main').append($messages);
+};
+
+var req = $.ajax({
+  url: parseUrl,
+  success: function(data) { 
+    displayMessages(data.results);
+  },
+  dataType: 'json'
+});
+
