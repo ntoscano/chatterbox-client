33049329e069e42c0e9f3f88123a6e418c4f7482
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 21:05:11 -0700

    Implement chatroom selection

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,31 +1,86 @@
 // YOUR CODE HERE:
 
-var parseUrl = 'https://api.parse.com/1/classes/chatterbox';
+$(document).ready(function(){
 
-var displayMessages = function(messages) {
-  var $messages = $('<div>');
-
-  _(messages).each(function(message) {
-    var $message = $('<p class="message">');
-    $message.text(message.text);
-    $messages.append($message);
-  });
-  removeMessages();
-  $('#main').append($messages);
-};
-
-var getMessages = function() {
-  $.ajax({
-    url: parseUrl,
-    success: function(data) { 
-      displayMessages(data.results);
+  window.app = {
+    roomFilter: null,
+    server: 'https://api.parse.com/1/classes/chatterbox',
+    init: function() {
+      app.fetch();
+      setInterval(app.fetch, 10000);
     },
-    dataType: 'json'
+    fetch: function() {
+      $.ajax({
+        url: app.server,
+        success: function(data) {
+          app.displayMessages(data.results);
+        },
+        dataType: 'json'
+      });
+    },
+    send: function(message) {
+      $.ajax({
+        type: "POST",
+        data: JSON.stringify(message),
+        success: function(messageData){
+        },
+        url: app.server,
+        dataType: 'json'
+      });
+    },
+    displayMessages: function(messages) {
+      var $messages = $('<div id="chats"></div>');
+      var $rooms = $('.rooms');
+      var rooms = {};
+
+      _(messages).each(function(message) {
+        rooms[message.roomname] = true;
+        if (!app.roomFilter || message.roomname === app.roomFilter) {
+          var $message = $('<p class="message">');
+          $message.text(message.username + ': ' + message.text);
+          $messages.append($message);
+        }
+      });
+
+      app.clearMessages();
+
+      $('#main').append($messages);
+
+      Object.keys(rooms).forEach(function(room) {
+        var $room = $('<option class="room"></option>');
+        $room.data('room', room);
+        $room.text(room);
+        $rooms.append($room);
+      });
+    },
+    clearMessages: function() {
+      $('.rooms').children().remove();
+      $('#chats').remove();
+    }
+  };
+
+  app.init();
+
+  $('.send-message').on('click', function() {
+    var user = $('.user-name').val();
+    var message = $('.user-message').val();
+
+    var messageData = {
+      username: user,
+      text: message,
+      roomname: 'lobby'
+    };
+
+    app.send(messageData);
+    app.fetch();
   });
-};
 
-var removeMessages = function() {
-  $('.message').remove();
-};
+  $('.rooms').on('change', function() {
+    var $room = $(this).find('option:selected');
+    console.log($room.data('room'));
+    app.roomFilter = $(this).data('room');
+    app.fetch();
+  });
 
-setInterval(getMessages, 1000);
+
+});
