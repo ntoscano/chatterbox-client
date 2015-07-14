a05ce67068849910cc53f6a97071b0e60b9e981c
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 17:01:23 -0700

    Implement automatic update

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,18 +6,26 @@ var displayMessages = function(messages) {
   var $messages = $('<div>');
 
   _(messages).each(function(message) {
-    var $message = $('<p>');
+    var $message = $('<p class="message">');
     $message.text(message.text);
     $messages.append($message);
   });
+  removeMessages();
   $('#main').append($messages);
 };
 
-var req = $.ajax({
-  url: parseUrl,
-  success: function(data) { 
-    displayMessages(data.results);
-  },
-  dataType: 'json'
-});
+var getMessages = function() {
+  $.ajax({
+    url: parseUrl,
+    success: function(data) { 
+      displayMessages(data.results);
+    },
+    dataType: 'json'
+  });
+};
 
+var removeMessages = function() {
+  $('.message').remove();
+};
+
+setInterval(getMessages, 1000);
